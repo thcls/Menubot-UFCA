b6b1247c56794cafb0660903cc2cc1f8d88de0af
Thiago Carlos <thiagocarlossilvapereira@gmail.com>
2023-06-30 22:25:56 -0300

    ➕ build: Variáveis de ambiente

--- assets/scripts/utils.py
+++ assets/scripts/utils.py
@@ -2,21 +2,16 @@ from json import load, dump
 import fitz
 from random import randint
 
-def menuToImage():
+def menuToImage() -> None:
     with fitz.open('assets/menus/menu.pdf') as pdf:
         page = pdf[0]
-        matrix = fitz.Matrix(5,5)
+        matrix = fitz.Matrix(4,4)
         img = page.get_pixmap(matrix=matrix,alpha=False).tobytes("png")
           
     with open('assets/img/menuImg.png', 'wb') as file:
         file.write(img)
 
-def getToken():
-    with open('assets/json/token.json',encoding='utf-8') as json_file:
-        token = load(json_file)
-    return token['token']
-
-def randomLines():
+def randomLines() -> str:
     with open('assets/json/lines.json',encoding='utf-8') as json_file:
         lines = load(json_file)
     return lines[str(randint(1, 48))]
@@ -47,6 +42,11 @@ def getChannelids():
 def setChannelids(channelsIds):
     with open('assets/json/channels.json') as json_file:
         dump(channelsIds, json_file)
-        
+
+def timefunction(config) -> None:
+    if config["menuInterval"] >= 7200:
+        config["menuInterval"] = 60
+    else:
+        config["menuInterval"] *= 2
 if __name__ == '__main__':
     menuToImage()
